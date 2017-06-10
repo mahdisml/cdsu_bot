8736ea3409fbae678c201f9315003345f9158730
amirbagh75 <zerodays75@gmail.com>
2017-06-10 23:25:39 +0430

    version 0.0.0

--- autoload.php
+++ autoload.php
@@ -4,14 +4,14 @@ require_once 'libs/Telegram.php';
 require_once 'libs/WebHookGet.php';
 require_once 'libs/Keyboard.php';
 date_default_timezone_set('Asia/Tehran');
-$telegram = new Telegram("281687006:AAFilS_mvN8NDEyW696sKqfekOXtxldXHPg");
+$telegram = new Telegram("");
 $data = new webHookGet($telegram);
 $keyboard = new keyboard();
 $database = new medoo([
     'database_type' => 'mysql',
     'database_name' => 'cdsu',
     'server' => '127.12.208.131',
-    'username' => 'adminehcDFex',
-    'password' => 'bEqk77i6i7Ei',
+    'username' => '',
+    'password' => '',
     'charset' => 'utf8mb4'
 ]);
